876429c19fbb733ab944a908697180a863fece3b
johnson-rl <johnson.rl@gmail.com>
2017-01-26 22:47:51 -0800

    cleaned up merge

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,16 +15,11 @@ class PetsController < ApplicationController
   end
 
   def new
-  # TODO: set up *new* method with data for new view
-
-  def new
     @pet = Pet.new
     # owner_id = params[:owner_id]
     # @owner = Owner.find_by (id: owner_id)
   end
 
-  def create
-  # TODO: set up *create* method with database interactions for create
   def create
   owner = Owner.find(params[:owner_id])
   new_pet = Pet.new(pet_params)
@@ -34,16 +29,13 @@ class PetsController < ApplicationController
     else
       flash[:error] = new.pet.errors.full_messages.join(", ")
       redirect_to new_owner_pet_path(owner)
-  end
+    end
   end
 
   # TODO: handle save errors and communicate issues to user
-    pet = Pet.create(pet_params)
-    owner = Owner.find_by(id: params[:owner_id])
-    owner.pets << pet
-    redirect_to pet_path(pet)
-  end
+
   private
+
   def pet_params
     params.require(:pet).permit(:name, :breed, :date_of_birth)
   end
--- db/migrate/20170125220333_add_owner_ref_to_pets.rb
+++
@@ -1,5 +0,0 @@
-class AddOwnerRefToPets < ActiveRecord::Migration[5.0]
-  def change
-        add_reference :pets, :owner, foreign_key: true
-  end
-end
